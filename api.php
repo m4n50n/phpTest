e8590c0f4386ae61b6198d77f5927655e8ddefae
Jose Clemente Garcia Rodriguez <josegarciarodriguez89@hotmail.com>
2022-05-24 10:36:33 +0200

    test

--- api.php
+++ api.php
@@ -8,13 +8,8 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 /* API TEST */
 $data = json_decode(file_get_contents("php://input"));
 
-if (!isset($data["peticion"])) {
-    http_response_code(400);
-    echo json_encode(array("response" => "Bad Request"));
-} else {
-    http_response_code(200);
-    echo json_encode(array("response" => datos()));
-}
+http_response_code(200);
+echo json_encode(array("response" => datos()));
 
 function datos()
 {
