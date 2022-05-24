ed009f185a44336fcc302b33eaa22fda87f7261b
Jose Clemente Garcia Rodriguez <josegarciarodriguez89@hotmail.com>
2022-05-24 10:02:36 +0200

    test

--- api.php
+++ api.php
@@ -3,11 +3,12 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 /* API TEST */
 $data = json_decode(file_get_contents("php://input"));
 
-if (!$data) {
+if (empty($data)) {
     http_response_code(400); 
     echo json_encode(array("respuesta" => "Petición incorrecta"));
     echo json_encode("No se ha recibido ningún dato");
