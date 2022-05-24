9c8614bdd64a49f50a4304d5e7b007a34125c691
Jose Clemente Garcia Rodriguez <josegarciarodriguez89@hotmail.com>
2022-05-24 10:44:06 +0200

    test

--- api.php
+++ api.php
@@ -2,11 +2,22 @@
 
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Methods: GET");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-/* API TEST */
-$data = json_decode(file_get_contents("php://input"));
+$data = json_decode(file_get_contents("php://input"), true);
+
+if (!isset($data["api_key"])) {
+    http_response_code(400);    
+    echo json_encode(array("response"=>"Bad Request"));
+    exit();
+}
+
+if (md5(date("d-m-Y")) !== md5($data["api_key"])) {
+    http_response_code(401);    
+    echo json_encode(array("response"=>"Unauthorized"));
+    exit();
+}
 
 http_response_code(200);
 echo json_encode(array("response" => datos()));
