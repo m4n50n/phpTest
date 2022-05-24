172a9395928dd15112efca964af079e6462c6f5e
Jose Clemente Garcia Rodriguez <josegarciarodriguez89@hotmail.com>
2022-05-24 09:42:10 +0200

    test

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
 
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+
 /* API TEST */
-echo json_encode("prueba");
+$data = json_decode(file_get_contents("php://input"));
+print_r($data);
 
 ?>
